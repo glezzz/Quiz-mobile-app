78382f874da88dc7af53c7bd291366c94b986cb2
glezzz <alexglezk@gmail.com>
2021-03-09 15:18:43 +0100

    working on questions view

--- app/src/main/java/com/myproject/flagquizapp/Constants.kt
+++ app/src/main/java/com/myproject/flagquizapp/Constants.kt
@@ -41,33 +41,40 @@ object Constants {
         )
         questionsList.add(que5)
 
-        val que1 = Question(
-            1, questionText,
-            R.drawable.ic_flag_of_argentina, "Argentina", "Australia", "Armenia",
-            "Austria", 1
+        val que6 = Question(
+            6, questionText,
+            R.drawable.ic_flag_of_new_zealand, "New Zealand", "South Africa", "Australia",
+            "Philippines", 1
         )
-        questionsList.add(que1)
+        questionsList.add(que6)
 
-        val que1 = Question(
-            1, questionText,
-            R.drawable.ic_flag_of_argentina, "Argentina", "Australia", "Armenia",
-            "Austria", 1
+        val que7 = Question(
+            7, questionText,
+            R.drawable.ic_flag_of_germany, "Belgium", "Italy", "Spain",
+            "Germany", 4
         )
-        questionsList.add(que1)
+        questionsList.add(que7)
 
-        val que1 = Question(
-            1, questionText,
-            R.drawable.ic_flag_of_argentina, "Argentina", "Australia", "Armenia",
-            "Austria", 1
+        val que8 = Question(
+            8, questionText,
+            R.drawable.ic_flag_of_fiji, "Thailand", "Galapagos", "Fiji",
+            "Bermuda", 3
         )
-        questionsList.add(que1)
+        questionsList.add(que8)
 
-        val que1 = Question(
-            1, questionText,
-            R.drawable.ic_flag_of_argentina, "Argentina", "Australia", "Armenia",
-            "Austria", 1
+        val que9 = Question(
+            9, questionText,
+            R.drawable.ic_flag_of_brazil, "Bolivia", "Brazil", "Colombia",
+            "Canada", 1
         )
-        questionsList.add(que1)
+        questionsList.add(que9)
+
+        val que10 = Question(
+            10, questionText,
+            R.drawable.ic_flag_of_australia, "New Zealand", "Tasmania", "Australia",
+            "USA", 1
+        )
+        questionsList.add(que10)
 
         return questionsList
 
--- app/src/main/java/com/myproject/flagquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/myproject/flagquizapp/QuizQuestionsActivity.kt
@@ -1,11 +1,15 @@
 package com.myproject.flagquizapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        val questionsList = Constants.getQuestions()
+        Log.i("Questions size", "${questionsList.size}")
     }
 }
