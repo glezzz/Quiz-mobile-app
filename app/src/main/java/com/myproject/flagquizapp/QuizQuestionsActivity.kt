53619732259e359dd6c4afeff6d50fbc292f887b
glezzz <alexglezk@gmail.com>
2021-03-10 14:01:41 +0100

    working on correct/wrong answer

--- app/src/main/java/com/myproject/flagquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/myproject/flagquizapp/QuizQuestionsActivity.kt
@@ -5,14 +5,17 @@ import android.graphics.Typeface
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.myproject.flagquizapp.databinding.ActivityQuizQuestionsBinding
 
+// View.OnClickListener: Interface definition for a callback to be invoked when a view is clicked
 class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityQuizQuestionsBinding
 
+    // Default and first question position
     private var mCurrentPosition: Int = 1
     private var mQuestionsList: ArrayList<Question>? = null
     private var mSelectedOptionPosition: Int = 0
@@ -30,26 +33,35 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         binding.tvOptionTwo.setOnClickListener(this)
         binding.tvOptionThree.setOnClickListener(this)
         binding.tvOptionFour.setOnClickListener(this)
+        binding.btnSubmit.setOnClickListener(this)
     }
 
+    /**
+     * Sets up the question to UI components
+     */
     private fun setQuestion() {
 
         mCurrentPosition = 1
+        // Getting the question from the list with the help of current position
         val question = mQuestionsList!![mCurrentPosition - 1]
 
         defaultOptionsView()
 
         binding.progressBar.progress = mCurrentPosition
+        // Updates de progress bar with current position
         binding.tvProgress.text = "$mCurrentPosition" + "/" + binding.progressBar.max
 
         binding.tvQuestion.text = question.question
         binding.ivImage.setImageResource(question.image)
-        binding.tvOptionOne.setText(question.optionOne)
-        binding.tvOptionTwo.setText(question.optionTwo)
-        binding.tvOptionThree.setText(question.optionThree)
-        binding.tvOptionFour.setText(question.optionFour)
+        binding.tvOptionOne.text = question.optionOne
+        binding.tvOptionTwo.text = question.optionTwo
+        binding.tvOptionThree.text = question.optionThree
+        binding.tvOptionFour.text = question.optionFour
     }
 
+    /**
+     * Sets default options view when the new question is loaded or when the answer is reselected
+     */
     private fun defaultOptionsView() {
 
         val options = ArrayList<TextView>()
@@ -68,6 +80,9 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    /**
+     * Called when a view has been clicked
+     */
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.tv_option_one -> {
@@ -82,14 +97,72 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
             R.id.tv_option_four -> {
                 selectedOptionView(binding.tvOptionFour, 4)
             }
+            R.id.btn_submit -> {
+                if (mSelectedOptionPosition == 0) {
+                    mCurrentPosition++
+
+                    when {
+                        mCurrentPosition <= mQuestionsList!!.size -> {
+                            setQuestion()
+                        }
+                        else -> {
+                            Toast.makeText(
+                                this,
+                                "You have successfully completed the quiz",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                    // User has selected an option
+                } else {
+                    // Get the selected answer from the array list
+                    val question = mQuestionsList?.get(mCurrentPosition - 1)
+                    // Check if selected answer matches correct answer
+                    if (question!!.correctAnswer != mSelectedOptionPosition) {
+                        answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    }
+                    answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
+                    // Transform submit button
+                    if (mCurrentPosition == mQuestionsList!!.size) {
+                        binding.btnSubmit.text = "FINISH"
+
+                    }else{
+                        binding.btnSubmit.text = "GO TO NEXT QUESTION"
+
+                    }
+                }
+            }
         }
     }
 
+    /**
+     * Sets up the correct/wrong answer view
+     */
+    private fun answerView(answer: Int, drawableView: Int) {
+        when (answer) {
+            1 -> {
+                binding.tvOptionOne.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            2 -> {
+                binding.tvOptionTwo.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            3 -> {
+                binding.tvOptionThree.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            4 -> {
+                binding.tvOptionFour.background = ContextCompat.getDrawable(this, drawableView)
+            }
+        }
+    }
+
+    /**
+     * Sets the view of selected option view
+     */
     private fun selectedOptionView(tv: TextView, selectedOptionNumber: Int) {
         defaultOptionsView()
         mSelectedOptionPosition = selectedOptionNumber
 
-        tv.setTextColor(Color.parseColor("@color/dark_gray"))
+        tv.setTextColor(Color.parseColor("#363A43"))
         // Set text bold of selected option
         tv.setTypeface(tv.typeface, Typeface.BOLD)
         tv.background = ContextCompat.getDrawable(
