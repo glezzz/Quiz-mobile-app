c6233a2e42bab7e0d16b04ba933f255ba26ef139
glezzz <alexglezk@gmail.com>
2021-03-10 12:46:46 +0100

    refactored to use view  binding

--- app/src/main/java/com/myproject/flagquizapp/MainActivity.kt
+++ app/src/main/java/com/myproject/flagquizapp/MainActivity.kt
@@ -3,23 +3,26 @@ package com.myproject.flagquizapp
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.myproject.flagquizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
-        val startButton: Button = findViewById(R.id.btn_start)
-        val etName: EditText = findViewById(R.id.et_name)
+//        val startButton: Button = findViewById(R.id.btn_start)
+//        val etName: EditText = findViewById(R.id.et_name)
 
-        startButton.setOnClickListener {
-            if (etName.text.toString().isEmpty()) {
+        binding.btnStart.setOnClickListener {
+            if (binding.etName.text.toString().isEmpty()) {
                 Toast.makeText(
                     this,
                     "Please, enter your name", Toast.LENGTH_SHORT
--- app/src/main/java/com/myproject/flagquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/myproject/flagquizapp/QuizQuestionsActivity.kt
@@ -1,44 +1,100 @@
 package com.myproject.flagquizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
-import android.util.Log
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.ProgressBar
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.myproject.flagquizapp.databinding.ActivityQuizQuestionsBinding
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityQuizQuestionsBinding
+
+    private var mCurrentPosition: Int = 1
+    private var mQuestionsList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz_questions)
+        binding = ActivityQuizQuestionsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val questionsList = Constants.getQuestions()
-        Log.i("Questions size", "${questionsList.size}")
+        mQuestionsList = Constants.getQuestions()
 
-        val currentPosition = 1
-        val question: Question? = questionsList[currentPosition - 1]
-        val progressBar: ProgressBar = findViewById(R.id.progressBar)
-        val tvProgress: TextView = findViewById(R.id.tv_progress)
-        val tvQuestion: TextView = findViewById(R.id.tv_question)
-        val ivImage: ImageView = findViewById(R.id.iv_image)
-        val tvOptionOne: EditText = findViewById(R.id.tv_option_one)
-        val tvOptionTwo: EditText = findViewById(R.id.tv_option_two)
-        val tvOptionThree: EditText = findViewById(R.id.tv_option_three)
-        val tvOptionFour: EditText = findViewById(R.id.tv_option_four)
+        setQuestion()
 
-        progressBar.progress = currentPosition
-        tvProgress.text = "$currentPosition" + "/" + progressBar.max
+        binding.tvOptionOne.setOnClickListener(this)
+        binding.tvOptionTwo.setOnClickListener(this)
+        binding.tvOptionThree.setOnClickListener(this)
+        binding.tvOptionFour.setOnClickListener(this)
+    }
 
-        tvQuestion.text = question!!.question
-        ivImage.setImageResource(question.image)
-        tvOptionOne.setText(question.optionOne)
-        tvOptionTwo.setText(question.optionTwo)
-        tvOptionThree.setText(question.optionThree)
-        tvOptionFour.setText(question.optionFour)
+    private fun setQuestion() {
 
+        mCurrentPosition = 1
+        val question = mQuestionsList!![mCurrentPosition - 1]
 
+        defaultOptionsView()
 
+        binding.progressBar.progress = mCurrentPosition
+        binding.tvProgress.text = "$mCurrentPosition" + "/" + binding.progressBar.max
 
+        binding.tvQuestion.text = question.question
+        binding.ivImage.setImageResource(question.image)
+        binding.tvOptionOne.setText(question.optionOne)
+        binding.tvOptionTwo.setText(question.optionTwo)
+        binding.tvOptionThree.setText(question.optionThree)
+        binding.tvOptionFour.setText(question.optionFour)
+    }
+
+    private fun defaultOptionsView() {
+
+        val options = ArrayList<TextView>()
+        options.add(0, binding.tvOptionOne)
+        options.add(1, binding.tvOptionTwo)
+        options.add(2, binding.tvOptionThree)
+        options.add(3, binding.tvOptionFour)
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.tv_option_one -> {
+                selectedOptionView(binding.tvOptionOne, 1)
+            }
+            R.id.tv_option_two -> {
+                selectedOptionView(binding.tvOptionTwo, 2)
+            }
+            R.id.tv_option_three -> {
+                selectedOptionView(binding.tvOptionThree, 3)
+            }
+            R.id.tv_option_four -> {
+                selectedOptionView(binding.tvOptionFour, 4)
+            }
+        }
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptionNumber: Int) {
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNumber
+
+        tv.setTextColor(Color.parseColor("@color/dark_gray"))
+        // Set text bold of selected option
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_option_border_bg
+        )
     }
 }
